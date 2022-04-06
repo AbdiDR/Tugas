6b25efdaf78149be857036b3faa5d91de5c947a7
AbdiDR <100252614+AbdiDR@users.noreply.github.com>
2022-04-06 12:54:43 +0700

    Add files via upload

---
+++ Android.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author DARUL ZUMANI
+ */
+public class Android extends InputNilai implements Hitung{
+    public Android (String nama, int nilaiSatu, int nilaiDua, int nilaiTiga){
+        super(nama, nilaiSatu, nilaiDua, nilaiTiga);
+    }
+    
+    public void inputNilaiSatu(int nilaiSatu) {
+        this.nilaiSatu = nilaiSatu;
+    }
+
+     public void inputNilaiDua(int nilaiDua) {
+        this.nilaiDua = nilaiDua;
+    }
+
+     public void inputNilaiTiga(int nilaiTiga) {
+        this.nilaiTiga = nilaiTiga;
+    }
+     
+    @Override
+    public double NilaiTotal() {
+         return (0.2 * super.nilaiSatu) + (0.5 * super.nilaiDua) + (0.3 * super.nilaiTiga);
+    }
+    
+    @Override
+    public String Keterangan() {
+        if(this.NilaiTotal() >= 85) 
+            return "LULUS\nSelamat kepada " + this.nama + " telah DITERIMA sebagai Androoid Development dengan nilai " + NilaiTotal() + "\n";
+        else 
+            return "GAGAL\nMohon maaf kepada  " + this.nama + " telah DITOLAK sebagai Android Development\n";
+    }
+}
